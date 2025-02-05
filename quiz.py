9397e2a275187b3d1688c5119cd2b65a9844b61c
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-02-05 16:02:34 +0100

    Mise en place du fichier de configuration
    - Mise en place du fichier config.json qui définie le nombre de question et le theme du quiz.
    - Correction du programme principal

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+import json
+
 # Simple quiz en utilisant les dictionnaires
 def main():
     questions={'Quel est la capitale du Pérou ? ':'Lima',
@@ -5,10 +7,16 @@ def main():
         'Comment dit on plage en espagnol ':'playa',
         'Quel est la longueur du canal de Nantes à Brest':'260',
     }
+
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = json.load(f)
+
     print ("*** Début du Quiz ***\n")
     nom = input (" Entrez votre nom: ").title()
     print ()
-    print("\nBien joué {0}, vous avez repondu à {1} de {2} questions.".format(nom, quiz(questions), len(questions)))
+    print(f"Thème du quiz : {config['theme']}")
+    print(f"Nombre de questions : {config['nombre_de_questions']}\n")
+    print("\nBien joué {0}, vous avez répondu correctement à {1} sur {2} questions.".format(nom, quiz(questions), len(questions)))
 
 def quiz(qs):
     points = 0
@@ -16,9 +24,9 @@ def quiz(qs):
         if input(qu).lower() == an.lower():
             points += 1
             print("Juste.")
-    else:
-        print("Oups, la bonne réponse est \"{}\".".format(an))
-        return points
+        else:
+            print("Oups, la bonne réponse est \"{}\".".format(an))
+    return points
     
 if __name__ == "__main__":
     main()
