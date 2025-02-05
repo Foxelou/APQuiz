b6dc368fc763f9d5bca02fafd41bcd9013c1f2f8
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-02-05 18:00:21 +0100

    fonction d'initialisation
    Création d'une fonction d'initialisation qui initialise les variables theme et nombre_de_questions

--- quiz.py
+++ quiz.py
@@ -1,21 +1,32 @@
 import json
 
-# Simple quiz en utilisant les dictionnaires
-def main():
-    questions={'Quel est la capitale du Pérou ? ':'Lima',
-        'Comment s\'appelle le petit du lion':'lionceau',
-        'Comment dit on plage en espagnol ':'playa',
-        'Quel est la longueur du canal de Nantes à Brest':'260',
-    }
+theme = "Culture Générale"
+nombre_de_questions = 10
 
+def initialiser_quiz():
+    global theme
+    global nombre_de_questions
+    
     with open('config.json', 'r', encoding='utf-8') as f:
         config = json.load(f)
 
+    theme = config['theme']
+    nombre_de_questions = config['nombre_de_questions']
+
+# Simple quiz en utilisant les dictionnaires
+def main():
+    questions={'Quel est la capitale du Pérou ? ':'Lima',
+        'Comment s\'appelle le petit du lion ? ':'lionceau',
+        'Comment dit on plage en espagnol ? ':'playa',
+        'Quel est la longueur du canal de Nantes à Brest ? ':'260',
+    }
+
     print ("*** Début du Quiz ***\n")
+    initialiser_quiz()
     nom = input (" Entrez votre nom: ").title()
     print ()
-    print(f"Thème du quiz : {config['theme']}")
-    print(f"Nombre de questions : {config['nombre_de_questions']}\n")
+    print(f"Thème du quiz : {theme}")
+    print(f"Nombre de questions : {nombre_de_questions}\n")
     print("\nBien joué {0}, vous avez répondu correctement à {1} sur {2} questions.".format(nom, quiz(questions), len(questions)))
 
 def quiz(qs):
